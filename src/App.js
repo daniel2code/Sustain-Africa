2467b4296db94e078d603f5e9809574e7ca6bdbf
9thtower <topfemo@gmail.com>
2021-07-06 00:25:58 +0100

    initial files structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import { Layout } from "antd";
+// import LoadingBar from "react-redux-loading-bar";
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import DealsList from "./pages/DealsList/DealsList";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Layout className="layout">
+        <div className="loading-bar">
+          {/* <LoadingBar
+            style={{
+              backgroundColor: "#1890ff",
+              height: "2px",
+              position: "fixed",
+              top: "0",
+            }}
+          /> */}
+        </div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={DealsList} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
