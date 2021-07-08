18b9d4167da996e673baf0f4578d950b8ea9c106
Daniel Osi <ikennaosi@outlook.com>
2021-07-07 18:29:22 -0700

    - install fontawesome - add icons to navbar - minor adjustments

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,36 @@
 import React from "react";
-import { Button } from "antd";
-import { Link } from "react-router-dom";
+import {Button} from "antd";
+import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import "./Navbar.scss";
 
 export default function Navbar() {
-  return (
-    <div className="navbar-container">
-      <div className="navbar-wrapper">
-        <div className="row-one">
-          <Button size="small" type="link">
-            <Link to="/login">login</Link>
-          </Button>
-          <Button type="primary" size="small">
-            <Link to="/register">register</Link>
-          </Button>
-        </div>
+    return (
+        <div className="navbar-container">
+            <div className="navbar-wrapper">
+                <div className="row-one">
+                    <Button size="small" type="link">
+                        <Link to="/login">login</Link>
+                    </Button>
+                    <Button type="primary" size="small">
+                        <Link to="/register">register</Link>
+                    </Button>
+                </div>
 
-        <div className="row-two">
-          <div className="top">
-            <Link to="/">sustain.africa</Link>
-          </div>
-          <div className="bottom">
-            alternative ways to move funds around the world.
-          </div>
+                <div className="row-two">
+                    <div className="top">
+                        <Link to="/">sustain.africa</Link>
+                    </div>
+                    <div className="bottom">
+                        alternative ways to move funds around the world.
+                    </div>
+                </div>
+                <div className="row-three">
+                    <div className="top"><FontAwesomeIcon icon="fa-envelope" /> help@sustain.africa</div>
+                    <div className="bottom"><FontAwesomeIcon icon={['fab', 'twitter']} /> @sustainafrica</div>
+                </div>
+            </div>
         </div>
-        <div className="row-three">support: help@sustain.africa</div>
-        <div className="row-three">twitter: @sustainafrica</div>
-      </div>
-    </div>
-  );
+    );
 }
