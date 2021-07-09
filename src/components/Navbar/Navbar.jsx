7edb70509a8ef39d7eaccb9756f7116cbe7985a9
Daniel Osi <ikennaosi@outlook.com>
2021-07-09 02:36:25 -0700

    Email modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,7 +28,6 @@ export default function Navbar() {
         </div>
         <div className="row-three">
           <div className="top">
-            {" "}
             email: help@sustain.africa
           </div>
           <div className="bottom">
