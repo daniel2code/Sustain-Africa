f6263ce66fe627059a18e906067c40ce324a780a
9thtower <topfemo@gmail.com>
2021-07-18 13:37:10 +0100

    setup API utils file

---
+++ src/utils/API.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: "https://sustain.herogloballogistics.com/factory",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+});
+
+export const bearerInstance = axios.create({
+  baseURL: "https://sustain.herogloballogistics.com/factory",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "multipart/form-data",
+  },
+});
