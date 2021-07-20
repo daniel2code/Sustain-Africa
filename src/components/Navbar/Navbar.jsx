ccc0539d6858a86e56fe47262adf1e08195dd912
9thtower <topfemo@gmail.com>
2021-07-20 18:18:39 +0100

    client-side logout added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Dropdown, Menu } from "antd";
 import { Link, useHistory, useLocation } from "react-router-dom";
-import { TwitterOutlined, LeftOutlined, DownOutlined } from "@ant-design/icons";
-import { useSelector } from "react-redux";
+import {
+  TwitterOutlined,
+  LeftOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import { useSelector, useDispatch } from "react-redux";
 
 import "./Navbar.scss";
 
 export default function Navbar() {
   const userState = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
   return (
@@ -32,10 +37,36 @@ export default function Navbar() {
             {userState?.profile ? (
               <>
                 <div className="balance">$10.39</div>
-                <div className="name">
-                  {userState?.profile?.user_name}{" "}
-                  <DownOutlined style={{ fontSize: "13px" }} />
-                </div>
+
+                <Dropdown
+                  overlay={
+                    <Menu>
+                      <Menu.Item key="0">Profile</Menu.Item>
+                      <Menu.Item key="1">Settings</Menu.Item>
+                      <Menu.Divider />
+                      <Menu.Item
+                        key="3"
+                        onClick={() => {
+                          dispatch({ type: "DESTROY_SESSION" });
+                          history.push("/");
+                        }}
+                        style={{ color: "#ed1450" }}
+                      >
+                        Logout
+                      </Menu.Item>
+                    </Menu>
+                  }
+                  trigger={["click"]}
+                  placement="bottomRight"
+                >
+                  <div className="name">
+                    {userState?.profile?.user_name}{" "}
+                    <EllipsisOutlined
+                      rotate="90"
+                      style={{ fontSize: "13px" }}
+                    />
+                  </div>
+                </Dropdown>
               </>
             ) : (
               <>
