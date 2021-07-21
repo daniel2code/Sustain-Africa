245707aad4ca37b64e0f7f891ad3f08e17fcb42c
9thtower <topfemo@gmail.com>
2021-07-21 15:12:57 +0100

    added deals switch bar

--- src/pages/DealsList/DealsList.jsx
+++ src/pages/DealsList/DealsList.jsx
@@ -1,5 +1,58 @@
 import React from "react";
+import { Select } from "antd";
+import { SwapOutlined } from "@ant-design/icons";
+
+import "./style-Deals.scss";
+
+const { Option } = Select;
 
 export default function DealsList() {
-  return <div>Deals List</div>;
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  return (
+    <div className="deals-container">
+      <div className="deals-wrapper">
+        <div className="switch-bar">
+          <div className="left">deals</div>
+          <div className="right">
+            <div>
+              <Select
+                defaultValue="paypal"
+                style={{ width: 120 }}
+                onChange={handleChange}
+              >
+                <Option value="bank">bank</Option>
+                <Option value="paypal">paypal</Option>
+                <Option value="cash">cash</Option>
+                <Option value="skrill">skrill</Option>
+                <Option value="venmo">venmo</Option>
+                <Option value="skrill">skrill</Option>
+              </Select>
+            </div>
+            <div className="arrows">
+              <SwapOutlined />
+            </div>
+            <div>
+              <Select
+                defaultValue="bitcoin"
+                style={{ width: 120 }}
+                onChange={handleChange}
+              >
+                <Option value="bitcoin">bitcoin</Option>
+                <Option value="paypal">paypal</Option>
+                <Option value="cash">cash</Option>
+                <Option value="skrill">skrill</Option>
+                <Option value="venmo">venmo</Option>
+                <Option value="skrill">skrill</Option>
+              </Select>
+            </div>
+          </div>
+        </div>
+
+        <div className="deals-list"></div>
+      </div>
+    </div>
+  );
 }
