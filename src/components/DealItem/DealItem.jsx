316398e8f66ae1ce59c595402ba524efefa89034
Daniel Osi <ikennaosi@outlook.com>
2021-09-12 03:57:53 -0700

    score icon

--- src/components/DealItem/DealItem.jsx
+++ src/components/DealItem/DealItem.jsx
@@ -141,7 +141,7 @@ export default function DealItem({ item }) {
         </div>
 
         <div className="deal-item-row-three">
-          <span className="username-green">@{item?.user_name_front}<span className="score-green">#{item?.a_score}</span></span>
+          <span className="username-green">@{item?.user_name_front}<span className="score-green"> {item?.a_score}/100</span></span>
         </div>
 
         <div className="deal-item-row-three">
