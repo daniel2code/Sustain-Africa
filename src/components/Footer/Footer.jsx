d6a7756e1349919299dc1c8cee5a5e998118084b
9thtower <topfemo@gmail.com>
2021-09-15 00:14:53 +0100

    footer cleanup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { TwitterOutlined } from "@ant-design/icons";
 
 import "./Footer.scss";
 
 export default function Footer() {
-  useEffect(() => {
-    window.addEventListener("scroll", listenToScroll);
-    return () => window.removeEventListener("scroll", listenToScroll);
-    //eslint-disable-next-line
-  }, []);
-
-  const [hideSecNav, setHideSecNav] = useState(true);
-
-  const listenToScroll = () => {
-    let heightToHideFrom = 135;
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop;
-
-    if (winScroll > heightToHideFrom) {
-      hideSecNav && setHideSecNav(false);
-    } else {
-      setHideSecNav(true);
-    }
-  };
   return (
     <div className="footer-container">
       <div className="footer-wrapper">
