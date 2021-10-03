eed9bc1cf57dc330811184b71442c481e85711b6
9thtower <topfemo@gmail.com>
2021-10-03 21:13:44 +0100

    test stream

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,6 +1,62 @@
-import React from "react";
-import "./message.scss";
+import React, { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import "stream-chat-css/dist/css/index.css";
 
-export default function Message() {
-  return <div>hello</div>;
-}
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const client = StreamChat.getInstance("nbr24ycjwyvc");
+
+const Message = () => {
+  const [clientReady, setClientReady] = useState(false);
+
+  useEffect(() => {
+    console.log();
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "dave-matthews",
+            name: "Dave Matthews",
+          },
+          "4"
+        );
+
+        setClientReady(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+
+  return (
+    <Chat client={client}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default Message;
