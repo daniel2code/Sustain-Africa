30e972c626a11475171e152914dec5836d165b62
9thtower <topfemo@gmail.com>
2021-10-18 19:21:14 +0100

    stream api test

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,16 +1,73 @@
-import React from "react";
-import { Avatar, Input, Button } from "antd";
+import React, { useState, useEffect } from "react";
+import {
+  Avatar,
+  // Input,
+  //  Button,
+} from "antd";
 import {
   LikeOutlined,
   DislikeOutlined,
-  RightOutlined,
+  // RightOutlined,
 } from "@ant-design/icons";
-
+import { useSelector } from "react-redux";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import "stream-chat-css/dist/css/index.css";
 import "./message.scss";
+import axios from "axios";
 
-const { TextArea } = Input;
+// const { TextArea } = Input;
 
 export default function Message() {
+  const username = useSelector((state) => state?.user?.userData?.user_name);
+
+  const [chatChannel, setChatChannel] = useState(null);
+
+  useEffect(() => {
+    initChat();
+    // eslint-disable-next-line
+  }, []);
+
+  const client = new StreamChat("twtrsx9dd48k");
+
+  async function initChat() {
+    async function generateToken() {
+      const { token } = (
+        await axios.get(
+          `http://sustain.herogloballogistics.com/chat/server.php?create-token=${username}`
+        )
+      ).data;
+      return token;
+    }
+    const token = await generateToken();
+
+    client.connectUser(
+      {
+        id: username,
+        name: username,
+      },
+      token
+    );
+
+    let channel = client.channel("messaging", "sustain-test-1", {
+      name: "sustain test",
+      members: [],
+      created_by_id: username,
+      session: 8,
+    });
+
+    setChatChannel(channel);
+    await channel.watch();
+  }
+
   return (
     <div className="message-page-container">
       <div className="header-container">
@@ -50,9 +107,19 @@ export default function Message() {
 
       <div className="message-content">
         <div className="chat-summary">direct chat &#9679; no issues raised</div>
+        <Chat client={client} theme="messaging light">
+          <Channel channel={chatChannel}>
+            <Window>
+              <ChannelHeader />
+              <MessageList />
+              <MessageInput />
+            </Window>
+            <Thread />
+          </Channel>
+        </Chat>
       </div>
 
-      <div className="message-footer">
+      {/* <div className="message-footer">
         <div className="wrapper">
           <div className="row-one">
             <TextArea rows={4} placeholder="type a message..." />
@@ -74,7 +141,7 @@ export default function Message() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
