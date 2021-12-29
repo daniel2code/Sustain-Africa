2b4ecf9d2caf11c6c486e57d4a68e402c65f9b44
Adebayo Fawaz <lordrisk@gmai.com>
2021-12-29 05:16:26 +0100

    fawaz latest

--- src/components/NotificationCard/NotificationCard.jsx
+++ src/components/NotificationCard/NotificationCard.jsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { format } from 'timeago.js';
+import { bearerInstance } from '../../utils/API';
 
 //  d_r - discussion request
 //  d_r_r - discussion request rejected
@@ -7,14 +11,80 @@ import { format } from 'timeago.js';
 //  d_c - discussion completed
 
 const NotificationCard = ({ data }) => {
+  const [senderDet, setSenderDet] = useState();
+  const user = useSelector(state => state.user.userData);
+
+  useEffect(() => {
+    const userid = user.id === data.sender ? data.receiver : data.sender;
+
+    bearerInstance.get(`/profile?id=${userid}`).then(res => {
+      setSenderDet({
+        user: res.data.profile_data[0].user_name,
+        userFront: res.data.profile_data[0].user_name_front,
+      });
+    });
+
+    return () => {
+      setSenderDet({
+        user: '',
+        userFront: '',
+      });
+    };
+  }, [data.sender, data.receiver, user.id]);
+
   return (
     <div className="notification-card">
-      <div></div>
+      {data.type === 'd_r' && (
+        <div>
+          <p>
+            <Link
+              to={`/user/${data.sender}/profile`}
+              className="notification-link"
+            >
+              @{senderDet?.user}
+            </Link>{' '}
+            wants to discuss with you regarding your{' '}
+            <Link to={`/deal/${data.deal_id}`} className="notification-link">
+              deal
+            </Link>
+          </p>
+        </div>
+      )}
+
+      {data.type === 'd_c' && (
+        <div>
+          <p>
+            <Link to={`/deal/${data.deal_id}`} className="notification-link">
+              deal
+            </Link>{' '}
+            with{' '}
+            {data.sender === user.id ? (
+              <Link
+                to={`/user/${data.receiver}/profile`}
+                className="notification-link"
+              >
+                @{senderDet?.user}
+              </Link>
+            ) : (
+              <Link
+                to={`/user/${data.sender}/profile`}
+                className="notification-link"
+              >
+                @{user.user_name}
+              </Link>
+            )}{' '}
+            completed successfully
+          </p>
+        </div>
+      )}
 
       <div style={{ display: 'flex' }}>
         {data.type === 'd_r' && (
-          <div style={{ display: 'flex', gap: '10px', marginBottom: '5px' }}>
-            <button className="notification-card-btn notification-card-btn-pink">
+          <div style={{ display: 'flex', marginBottom: '5px' }}>
+            <button
+              style={{ marginRight: '20px' }}
+              className="notification-card-btn notification-card-btn-pink"
+            >
               accept
             </button>
 
@@ -24,7 +94,7 @@ const NotificationCard = ({ data }) => {
           </div>
         )}
         {data.type === 'd_c' && (
-          <div style={{ display: 'flex', gap: '10px', marginBottom: '5px' }}>
+          <div style={{ display: 'flex', marginBottom: '5px' }}>
             <button className="notification-card-btn notification-card-btn-pink">
               review
             </button>
--- src/pages/Notification/Notifiaction.jsx
+++ src/pages/Notification/Notifiaction.jsx
@@ -3,10 +3,13 @@ import './Notification.scss';
 import { bearerInstance } from '../../utils/API';
 import Loader from './../../components/Loader/Loader';
 import NotificationCard from '../../components/NotificationCard/NotificationCard';
+import { useSelector } from 'react-redux';
 
 export default function Notification() {
   const [notifications, setNotification] = useState();
   const [loading, setLoading] = useState(true);
+
+  const userId = useSelector(state => state.user.userData.id);
 
   useEffect(() => {
     setLoading(true);
@@ -24,17 +27,27 @@ export default function Notification() {
       });
   }, []);
 
+  //  d_r - discussion request - only reciever sees this
+  //  d_r_r - discussion request rejected - only sender see this
+  //  c_r - connection request
+  //  c_a - connection request accepted
+  //  d_c - discussion completed - both parties sees this
+
   return (
     <div className="notification">
       <div className="notification-wrapper">
-        <h1>Notifications</h1>
+        <h1>notifications</h1>
+
         {loading ? (
           <Loader />
         ) : (
           <div className="notification-body">
-            {notifications.map(cur => (
-              <NotificationCard key={cur.id} data={cur} />
-            ))}
+            {notifications.map(cur =>
+              cur.type === 'd_r' && userId === cur.sender ? null : cur.type ===
+                  'd_r_r' && userId === cur.reciever ? null : (
+                <NotificationCard key={cur.id} data={cur} />
+              )
+            )}
           </div>
         )}
       </div>
