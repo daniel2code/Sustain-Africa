1a3e56767c98037432846937afefa90bf519ad9d
Adebayo Fawaz <lordrisk@gmai.com>
2021-12-30 04:09:59 +0100

    latest

--- src/components/NotificationCard/NotificationCard.jsx
+++ src/components/NotificationCard/NotificationCard.jsx
@@ -4,6 +4,14 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { format } from 'timeago.js';
 import { bearerInstance } from '../../utils/API';
+import { message, Tooltip, Modal, Button } from 'antd';
+import {
+  LikeOutlined,
+  DislikeOutlined,
+  ArrowRightOutlined,
+  EllipsisOutlined,
+  ExclamationCircleOutlined,
+} from '@ant-design/icons';
 
 //  d_r - discussion request  - handled
 //  d_c - discussion completed - handled
@@ -15,6 +23,7 @@ import { bearerInstance } from '../../utils/API';
 const NotificationCard = ({ data }) => {
   const [senderDet, setSenderDet] = useState();
   const user = useSelector(state => state.user.userData);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const userid = user.id === data.sender ? data.receiver : data.sender;
@@ -42,111 +51,134 @@ const NotificationCard = ({ data }) => {
     };
   }, []);
 
-  return (
-    <div className="notification-card">
-      {(data.type === 'd_r' ||
-        data.type === 'd_r_r' ||
-        data.type === 'n_r') && (
-        <div>
-          <p
-            style={{
-              marginBottom: (data.type === 'd_r_r' || data.type === 'n_r') && 0,
-            }}
-          >
-            <Link
-              to={`/user/${data.sender}/profile`}
-              className="notification-link username-green"
-            >
-              @{senderDet?.userFront}
-            </Link>{' '}
-            {dealWriteUp[data.type]}
-            <Link
-              to={`/deal/${data.deal_id}`}
-              className="notification-link username-green"
-            >
-              deal
-            </Link>
-          </p>
-        </div>
-      )}
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-      {data.type === 'd_c' && (
-        <div>
-          <p>
-            <Link
-              to={`/deal/${data.deal_id}`}
-              className="notification-link username-green"
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+
+      <div className="notification-card">
+        {(data.type === 'd_r' ||
+          data.type === 'd_r_r' ||
+          data.type === 'n_r') && (
+          <div>
+            <p
+              style={{
+                marginBottom:
+                  (data.type === 'd_r_r' || data.type === 'n_r') && 0,
+              }}
             >
-              deal
-            </Link>{' '}
-            with{' '}
-            {data.sender === user.id && (
-              <Link
-                to={`/user/${data.receiver}/profile`}
-                className="notification-link username-green"
-              >
-                @{senderDet?.userFront}
-              </Link>
-            )}
-            {data.receiver === user.id && (
               <Link
                 to={`/user/${data.sender}/profile`}
                 className="notification-link username-green"
               >
                 @{senderDet?.userFront}
+              </Link>{' '}
+              {dealWriteUp[data.type]}
+              <Link
+                to={`/deal/${data.deal_id}`}
+                className="notification-link username-green"
+              >
+                deal
               </Link>
-            )}{' '}
-            completed successfully
-          </p>
-        </div>
-      )}
-
-      <div style={{ display: 'flex' }}>
-        {data.type === 'd_r' && (
-          <div style={{ display: 'flex', marginBottom: '5px' }}>
-            <button
-              style={{ marginRight: '10px' }}
-              className={`notification-card-btn notification-card-btn-pink${
-                data.accepted || data.rejected ? ' disabled' : ''
-              }`}
-              // disabled when the deal is
-              disabled={!(data.accepted || data.rejected)}
-            >
-              accept
-            </button>
-
-            <button className="notification-card-btn notification-card-btn-out">
-              reject
-            </button>
+            </p>
           </div>
         )}
+
         {data.type === 'd_c' && (
-          <div style={{ display: 'flex', marginBottom: '5px' }}>
-            <button
-              className={`notification-card-btn notification-card-btn-pink${
-                data.accepted || data.rejected ? '' : ''
-              }`}
-              // disabled when the deal is
-              disabled={!(data.accepted || data.rejected)}
-            >
-              review
-            </button>
+          <div>
+            <p>
+              <Link
+                to={`/deal/${data.deal_id}`}
+                className="notification-link username-green"
+              >
+                deal
+              </Link>{' '}
+              with{' '}
+              {data.sender === user.id && (
+                <Link
+                  to={`/user/${data.receiver}/profile`}
+                  className="notification-link username-green"
+                >
+                  @{senderDet?.userFront}
+                </Link>
+              )}
+              {data.receiver === user.id && (
+                <Link
+                  to={`/user/${data.sender}/profile`}
+                  className="notification-link username-green"
+                >
+                  @{senderDet?.userFront}
+                </Link>
+              )}{' '}
+              completed successfully
+            </p>
           </div>
         )}
 
-        <span
-          style={{
-            fontSize: '11px',
-            color: 'rgb(153, 153, 153)',
-            alignSelf: 'flex-end',
-            marginLeft: 'auto',
-          }}
-        >
-          {format(data?.created_at).split(' ')[0]}
-          {format(data?.created_at).split(' ')[1].substring(0, 1)}
-        </span>
+        <div style={{ display: 'flex' }}>
+          {data.type === 'd_r' && (
+            <div style={{ display: 'flex', marginBottom: '5px' }}>
+              <button
+                style={{ marginRight: '10px' }}
+                className={`notification-card-btn notification-card-btn-pink${
+                  data.accepted || data.rejected ? ' disabled' : ''
+                }`}
+                // disabled when the deal is
+                // disabled={!(data.accepted || data.rejected)}
+                onClick={() => setIsModalVisible(true)}
+              >
+                accept
+              </button>
+
+              <button className="notification-card-btn notification-card-btn-out">
+                reject
+              </button>
+            </div>
+          )}
+          {data.type === 'd_c' && (
+            <div style={{ display: 'flex', marginBottom: '5px' }}>
+              <button
+                className={`notification-card-btn notification-card-btn-pink${
+                  data.accepted || data.rejected ? '' : ''
+                }`}
+                // disabled when the deal is
+                disabled={!(data.accepted || data.rejected)}
+              >
+                review
+              </button>
+            </div>
+          )}
+
+          <span
+            style={{
+              fontSize: '11px',
+              color: 'rgb(153, 153, 153)',
+              alignSelf: 'flex-end',
+              marginLeft: 'auto',
+            }}
+          >
+            {format(data?.created_at).split(' ')[0]}
+            {format(data?.created_at).split(' ')[1].substring(0, 1)}
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
