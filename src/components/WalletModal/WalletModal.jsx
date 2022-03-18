7290c398f570496ebf4a30382f6bb996347e5c15
pherwerz <lordrisk101@gmail.com>
2022-03-18 07:28:16 +0100

    latest

--- src/components/WalletModal/WalletModal.jsx
+++ src/components/WalletModal/WalletModal.jsx
@@ -1,7 +1,8 @@
 import './WalletModal.scss';
 import { createPortal } from 'react-dom';
-import { CloseOutlined } from '@ant-design/icons';
+import { CloseOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { ReactComponent as Send } from '../../assets/send.svg';
+import { Alert, Button } from 'antd';
 
 const WalletModal = ({ send, close }) => {
   return createPortal(
@@ -13,14 +14,57 @@ const WalletModal = ({ send, close }) => {
           <CloseOutlined />
         </button>
 
-        {send ? null : (
-          <div>
-            <div style={{ color: '#000' }}>
-              <Send />
-            </div>
-            <div></div>
+        <div className="walletModal-box">
+          <div
+            className={send ? '' : 'walletModal-recieve-icon'}
+            style={{
+              color: 'rgb(255,213,0)',
+              marginRight: '5px',
+            }}
+          >
+            <Send style={{ height: '25px', width: '25px' }} />
           </div>
-        )}
+
+          {send ? null : (
+            <div className="walletModal-recieve walletModal-box-left">
+              <h3>receive bitcoin</h3>
+
+              <h4>your bitcoin address</h4>
+
+              <p>use this address to deposit bitcoin (btc) tokens:</p>
+
+              <p className="walletModal-address">
+                38p1GxDgz9GMqA4t9oe5Cpe1SmwhsSj
+              </p>
+
+              <div style={{ marginBottom: '20px' }}>
+                <Button type="primary">copy address </Button>
+
+                <Button type="text" style={{ color: '#ed1450' }}>
+                  get new
+                </Button>
+              </div>
+
+              <Alert
+                message="note: make sure you only send btc tokens on the bitcoin network to avoid fund loss!"
+                type="error"
+                showIcon
+                icon={
+                  <ExclamationCircleOutlined
+                    style={{ color: '#ed1450', marginTop: '5px' }}
+                  />
+                }
+                style={{
+                  color: '#ed1450',
+                  alignItems: 'flex-start',
+                  border: '2px solid #ed1450',
+                  borderRadius: '10px',
+                  backgroundColor: '#ed145040',
+                }}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>,
     document.getElementById('wallet-modal')
