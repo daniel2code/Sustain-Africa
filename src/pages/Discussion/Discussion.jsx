a3a903aead67fa682d1340aa17b09ac765a49fe5
Daniel Osi <ikennaosi@outlook.com>
2022-05-26 00:38:38 -0700

    discuss modal UI adjustments

--- src/pages/Discussion/Discussion.jsx
+++ src/pages/Discussion/Discussion.jsx
@@ -264,6 +264,7 @@ export default function Discussion() {
                       </Tag>
                     </div>
                   </div>
+
                   </div>
                 </Window>
               </Channel>
